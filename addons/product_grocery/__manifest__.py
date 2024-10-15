2848220c97f22222a397c727055120f754e0d2a0
AlphaBaiter <alpha.baiter.tech@gmail.com>
2024-10-15 20:11:07 +0000

    add product grocery

--- addons/product_grocery/__manifest__.py
+++ addons/product_grocery/__manifest__.py
@@ -18,7 +18,7 @@ Long description of module's purpose
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','product'],
+    'depends': ['product'],
 
     # always loaded
     'data': [
@@ -30,5 +30,7 @@ Long description of module's purpose
     'demo': [
         'demo/demo.xml',
     ],
+
+    'installable': 'true',
 }
 
