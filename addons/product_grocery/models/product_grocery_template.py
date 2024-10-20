b3fd2c7b0c931c181257ebd4c3bef1358f6365a0
AlphaBaiter <alpha.baiter.tech@gmail.com>
2024-10-20 03:15:52 +0300

    from pc

--- addons/product_grocery/models/product_grocery_template.py
+++ addons/product_grocery/models/product_grocery_template.py
@@ -6,4 +6,4 @@ from odoo import models, fields, api
 class ProductGroceryTemplate(models.Model):
     _name = 'product.template_grocery'
     _description = 'Product'
-    _inherits = ['product.template']
+   # _inherits = ['product.template']
